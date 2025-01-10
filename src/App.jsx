c19cdd48b00cc4952c93df5184f1c0277ea80345
himel mahmud <soft.mahmud@gmail.com>
2025-01-10 22:41:31 +0600

    update task 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl text-green-500 flex justify-center mt-80 bg-blue-300 p-5">
+      <h1 className="text-3xl text-green-700 flex justify-center mt-80 bg-blue-300 p-5">
         We are here to learn React from tapaScript.
       </h1>
     </>
